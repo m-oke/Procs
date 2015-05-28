8c117c0d54279f1ba40754f7826867b30009f6bb
m-oke <s1420616@u.tsukuba.ac.jp>
2015-05-28 16:06:33 +0900

    Add seed and Change schema.
    Add seed for test user. Change devise schema to disable mail confimation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
 
   has_and_belongs_to_many :lessons
 
--- db/schema.rb
+++ db/schema.rb
@@ -86,10 +86,6 @@ ActiveRecord::Schema.define(version: 20150527055613) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.string   "confirmation_token",     limit: 255
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",      limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
   end
