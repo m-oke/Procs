0f06cd14fafe3f2ff46eedf3c9949e1ef8b28c49
m-oke <s1420616@u.tsukuba.ac.jp>
2015-06-02 09:44:10 +0900

    Change db column string to binary.

--- db/migrate/20150522034105_create_lessons.rb
+++ db/migrate/20150522034105_create_lessons.rb
@@ -2,7 +2,7 @@ class CreateLessons < ActiveRecord::Migration
   def change
     create_table :lessons do |t|
       t.string :name, null: false
-      t.string :description
+      t.binary :description, :limit => 1000.kilobyte
       t.string :term
       t.integer :date
       t.string :period
--- db/migrate/20150522034444_create_questions.rb
+++ db/migrate/20150522034444_create_questions.rb
@@ -3,11 +3,11 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
       t.string :title, null: false
       t.integer :lesson_id, null: false
-      t.string :content
+      t.binary :content, :limit => 1000.kilobyte
       t.datetime :start_time, null: false
       t.datetime :end_time
-      t.string :input_description
-      t.string :output_description
+      t.binary :input_description, :limit => 1000.kilobyte
+      t.binary :output_description, :limit => 1000.kilobyte
       t.integer :run_time_limit
       t.integer :memory_usage_limit
       t.integer :cpu_usage_limit
