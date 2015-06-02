77932d7e3929982f8d46a879003d65bf8f7ac88a
m-oke <s1420616@u.tsukuba.ac.jp>
2015-06-02 09:23:19 +0900

    Fix db column name.

--- db/migrate/20150522034416_create_user_lessons.rb
+++ db/migrate/20150522034416_create_user_lessons.rb
@@ -2,7 +2,7 @@ class CreateUserLessons < ActiveRecord::Migration
   def change
     create_table :user_lessons do |t|
       t.string :id_number, null: false
-      t.integer :class_id, null: false
+      t.integer :lesson_id, null: false
 
       t.timestamps null: false
     end
--- db/migrate/20150522034444_create_questions.rb
+++ db/migrate/20150522034444_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.string :title, null: false
-      t.integer :class_id, null: false
+      t.integer :lesson_id, null: false
       t.string :content
       t.datetime :start_time, null: false
       t.datetime :end_time
