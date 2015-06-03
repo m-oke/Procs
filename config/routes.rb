ad821d7c4b9a1b802ba7b5f958d62afafb435f60
m-oke <s1420616@u.tsukuba.ac.jp>
2015-06-03 16:31:03 +0900

    Add views and controllers of devise to project.
    Add views and controllers of devise to override signin and signup pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root :to => 'lessons#index'
+
+  devise_for :users, :controllers => {
+    :sessions => 'users/sessions',
+    :registrations => 'users/registrations',
+    :passwords => 'users/passwords'
+  }
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
