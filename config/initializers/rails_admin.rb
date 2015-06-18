5f6b7088d755e43c8774cfa07a9f32f57e327311
m-oke <s1420616@u.tsukuba.ac.jp>
2015-06-18 13:09:45 +0900

    Modify rails_admin User edit/create page for roles_mask.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -31,4 +31,64 @@ RailsAdmin.config do |config|
     # history_index
     # history_show
   end
+
+  config.model User do
+    create do
+      field :student_number
+      field :name do
+        required true
+      end
+      field :nickname do
+        required true
+      end
+      field :roles do
+        required true
+        partial 'roles_form'
+      end
+      field :email do
+        required true
+      end
+      field :password
+      field :password_confirmation do
+        required true
+      end
+      field :reset_password_sent_at
+      field :remember_created_at
+      field :sign_in_count
+      field :current_sign_in_at
+      field :last_sign_in_at
+      field :current_sign_in_ip
+      field :last_sign_in_ip
+      field :user_lessons
+      field :lessons
+      field :answers
+      field :questions
+    end
+
+    update do
+      field :student_number
+      field :name
+      field :nickname
+      field :roles do
+        partial 'roles_form'
+      end
+      field :email
+      field :password do
+        required false
+      end
+      field :password_confirmation
+      field :reset_password_sent_at
+      field :remember_created_at
+      field :sign_in_count
+      field :current_sign_in_at
+      field :last_sign_in_at
+      field :current_sign_in_ip
+      field :last_sign_in_ip
+      field :user_lessons
+      field :lessons
+      field :answers
+      field :questions
+    end
+  end
+
 end
