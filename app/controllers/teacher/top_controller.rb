d3a693f28fc35507877ffa79d622ad12fa6cbefa
tokyolindh <tokyolindh@gmail.com>
2015-06-18 22:14:41 +0900

    name space of controller is deleted

--- app/controllers/teacher/top_controller.rb
+++
@@ -1,5 +0,0 @@
-class Teacher::TopController < ApplicationController
-  def index
-    render action: 'index'
-  end
-end
--- test/controllers/teacher/question_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Teacher::QuestionControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
