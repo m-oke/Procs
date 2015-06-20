5ab70cd9698235e48054e8fc045e924d04dde9d9
tokyolindh <tokyolindh@gmail.com>
2015-06-20 10:39:15 +0900

    before change the sample answer  inputs style

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,5 +2,6 @@ class LessonsController < ApplicationController
   def index
     @question = Question.new
     @sample = Sample.new
+    @test_data = TestDatum.new
   end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,6 +9,9 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.new(params[:question])
+    @sample = Sample.new(params[:sample])
+    @test_data = TestDatum.new(params[:test_data])
+
     if @question.save
       flash.notice='問題登録しました'
       redirect_to :controller => 'lessons', action:'index'
