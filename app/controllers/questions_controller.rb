d62ba337322850246f8e84c48dc9fc8be35f6a1b
m-oke <s1420616@u.tsukuba.ac.jp>
2015-06-22 13:32:54 +0900

    Add questions index link to navigation bar.
    Add redirect to root when user don't join the lesson.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,15 @@
+# -*- coding: utf-8 -*-
 class QuestionsController < ApplicationController
   def index
-    params[:lesson_id] ? @lesson = Lesson.find(params[:lesson_id]) : @lesson = nil
+    @lesson = nil
+    if params[:lesson_id]
+      @lesson = Lesson.find(params[:lesson_id])
+      ul = UserLesson.find_by(:user_id => current_user.id, :lesson_id => @lesson.id)
+      if ul.nil?
+        redirect_to root_path, :alert => "該当する授業に参加していません．"
+      end
+    end
+
     @lesson ? @questions = @lesson.question : @questions = Question.all
   end
 
