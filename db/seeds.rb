beb87a8fc55cfa0ae900a78918ebf03be7ed4a5e
m-oke <s1420616@u.tsukuba.ac.jp>
2015-11-17 06:18:13 +0900

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -67,7 +67,39 @@ end
 # 問題
 # 過去の問題
 Question.create(:title => "the 3n+1 problem",
-                :content => "詳しい内容はLinkのページを参照してくださいhttp://uva.onlinejudge.org/external/1/100.html　; 回答の参考はhttps://tausiq.wordpress.com/2008/12/09/uva-100-the-3n-1-problem/　です",
+                :content => "
+Problems in Computer Science are often classified as belonging to a certain class of problems (e.g., NP, Unsolvable, Recursive). In this problem you will be analyzing a property of an algorithm whose classification is not known for all possible inputs.
+
+The Problem
+
+Consider the following algorithm:
+
+
+1.  input n
+
+2.  print n
+
+
+3.  if n = 1 then STOP
+
+
+4.   if n is odd then  tex2html_wrap_inline44
+
+
+5.   else  tex2html_wrap_inline46
+
+
+6.  GOTO 2
+
+
+
+Given the input 22, the following sequence of numbers will be printed 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
+
+It is conjectured that the algorithm above will terminate (when a 1 is printed) for any integral input value. Despite the simplicity of the algorithm, it is unknown whether this conjecture is true. It has been verified, however, for all integers n such that 0 < n < 1,000,000 (and, in fact, for many more numbers than this.)
+
+Given an input n, it is possible to determine the number of numbers printed (including the 1). For a given n this is called the cycle-length of n. In the example above, the cycle length of 22 is 16.
+
+For any two numbers i and j you are to determine the maximum cycle length over all numbers between i and j.",
                 :input_description => "The input will consist of a series of pairs of integers i and j, one pair of integers per line. All integers will be less than 1,000,000 and greater than 0.
 You should process all pairs of integers and for each pair determine the maximum cycle length over all integers between and including i and j.
 You can assume that no operation overflows a 32-bit integer.",
