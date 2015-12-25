e85decb6bfd046fc8644b924a1b3a683d8c54c45
m-oke <s1420616@u.tsukuba.ac.jp>
2015-12-25 16:37:22 +0900

    Fix typo

--- scripts/install.sh
+++ scripts/install.sh
@@ -399,7 +399,7 @@ do_install(){
             echo 'We are unable to find either "sudo" or "su" available to make this happen.'
             exit 1
         fi
-    fip
+    fi
 
 
     # ディレクトリの設定
@@ -421,7 +421,7 @@ do_install(){
     start_sidekiq
     echo "Install completed!"
     echo "Please relogin, or restart server for using docker."
-    echo "Then please run ${dir}/script/start.sh to start server."
+    echo "Then please run $dir/script/start.sh to start server."
     exit 0
 }
 
