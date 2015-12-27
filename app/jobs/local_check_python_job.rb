89a1791935b68b45231662cea66ce239d724534a
shentu741 <shentu741@gmail.com>
2015-12-27 11:40:47 +0900

    自分との比較情報を排除する

--- app/jobs/local_check_python_job.rb
+++ app/jobs/local_check_python_job.rb
@@ -34,10 +34,15 @@ class LocalCheckPythonJob < ActiveJob::Base
             @compare_file = UPLOADS_ANSWERS_PATH.join(s.id.to_s, lesson_question.id.to_s, compare_answer.file_name)
             @compare_name = compare_answer.file_name
             @compare_path = @compare_file.to_s
+            @target_line_temp = ""
+            @compare_line_temp = ""
             @target_line = ""
             @compare_line = ""
             @check_line = 0
             @plagiarism_percentage = 0.0
+            @file_name1 = nil
+            @file_name2 = nil
+            @file_name_flag = nil
 
 
             # 類似度検出
@@ -59,64 +64,64 @@ class LocalCheckPythonJob < ActiveJob::Base
                 # Take the count of check line
                 check_line_right = line.rindex(" tokens=") - 2
                 @check_line = line[20..check_line_right].to_i
-
-                check_count += 1
               end
-              if line.include?(" path=\"" + @target_path)
-                # Take the line No. in target file which is checked
-                target_line_right = line.rindex(" path=\"" + @target_path) - 2
-                target_line_first_temp = line[12..target_line_right]
-                target_line_first = target_line_first_temp.to_i
+              if line.include?("<file line=") && @file_name_flag.nil?
+                if line.include?(@target_path)
+                  file_name1_left = line.rindex(" path=") + 7
+                  file_name1_right = line.rindex(@target_path) + @target_path.size - 1
+                  @file_name1 = line[file_name1_left..file_name1_right]
+                  @file_name_flag = "target"
 
-                # マークラインに空行とコメント行を追加
-                tar_check_line = @check_line
-                tar_file = File.open(@target_path, 'r:utf-8' )
-                k = 0
-                flag = false
-                tar_file.each_with_index do |l, j|
-                  k += 1
-                  if j == target_line_first
-                    flag = true
+                  set_target_line(line)
+                elsif line.include?(@compare_path)
+                  file_name1_left = line.rindex(" path=") + 7
+                  file_name1_right = line.rindex(@compare_path) + @compare_path.size - 1
+                  @file_name1 = line[file_name1_left..file_name1_right]
+                  @file_name_flag = "compare"
+
+                  set_compare_line(line)
+                end
+              elsif line.include?("<file line=") && @file_name_flag.present?
+                if line.include?(@target_path)
+                  if @file_name_flag == "target"
+                    @file_name1 = nil
+                    @file_name_flag = nil
+                    @check_line = 0
+                    @target_line_temp = ""
+                  elsif @file_name_flag == "compare"
+                    file_name2_left = line.rindex(" path=") + 7
+                    file_name2_right = line.rindex(@target_path) + @target_path.size - 1
+                    @file_name2 = line[file_name2_left..file_name2_right]
+
+                    set_target_line(line)
+                    @target_line << @target_line_temp
+                    @compare_line << @compare_line_temp
+                    check_count += 1
                   end
-                  if k == tar_check_line
-                    break
-                  end
-                  if flag
-                    if l.strip.chars[0].nil? || l.strip.chars[0] == "#"
-                      tar_check_line += 1
-                    end
+                elsif line.include?(@compare_path)
+                  if @file_name_flag == "compare"
+                    @file_name1 = nil
+                    @file_name_flag = nil
+                    @check_line = 0
+                    @compare_line_temp = ""
+                  elsif @file_name_flag == "target"
+                    file_name2_left = line.rindex(" path=") + 7
+                    file_name2_right = line.rindex(@compare_path) + @compare_path.size - 1
+                    @file_name2 = line[file_name2_left..file_name2_right]
+
+                    set_compare_line(line)
+                    @target_line << @target_line_temp
+                    @compare_line << @compare_line_temp
+                    check_count += 1
                   end
                 end
-                @target_line << target_line_first.to_s + "-" + (target_line_first + tar_check_line).to_s + ";"
+                @check_line = 0
+                @file_name_flag = nil
+                @file_name1 = nil
+                @file_name2 =nil
+                @target_line_temp = ""
+                @compare_line_temp = ""
               end
-              if line.include?(" path=\"" + @compare_path)
-                # Take the line No. in compare file which is checked
-                compare_line_right = line.rindex(" path=\"" + @compare_path) - 2
-                compare_line_first_temp = line[12..compare_line_right]
-                compare_line_first = compare_line_first_temp.to_i
-
-                # マークラインに空行とコメント行を追加
-                com_check_line = @check_line
-                com_file = File.open(@compare_path, 'r:utf-8' )
-                k = 0
-                flag = false
-                com_file.each_with_index do |l, j|
-                  k += 1
-                  if j == compare_line_first
-                    flag = true
-                  end
-                  if k == com_check_line
-                    break
-                  end
-                  if flag
-                    if l.strip.chars[0].nil? || l.strip.chars[0] == "#"
-                      com_check_line += 1
-                    end
-                  end
-                end
-                @compare_line << compare_line_first.to_s + "-" + (compare_line_first + com_check_line).to_s + ";"
-              end
-
             end
             # チェック実行で生成したファイルを削除
             File.delete(UPLOADS_ANSWERS_PATH.to_s + "/" + user_id.to_s + "_" + s.id.to_s + "_local_py_code.txt")
@@ -157,6 +162,62 @@ class LocalCheckPythonJob < ActiveJob::Base
     answer.local_plagiarism_percentage = local_result[0][4]
     answer.save
 
+  end
+
+  def set_target_line(line)
+    # Take the line No. in target file which is checked
+    target_line_right = line.rindex(" path=\"" + @target_path) - 2
+    target_line_first_temp = line[12..target_line_right]
+    target_line_first = target_line_first_temp.to_i
+
+    # マークラインに空行とコメント行を追加
+    tar_check_line = @check_line
+    tar_file = File.open(@target_path, 'r:utf-8' )
+    k = 0
+    flag = false
+    tar_file.each_with_index do |l, j|
+      k += 1
+      if j == target_line_first
+        flag = true
+      end
+      if k == tar_check_line
+        break
+      end
+      if flag
+        if l.strip.chars[0].nil? || l.strip.chars[0] == "#"
+          tar_check_line += 1
+        end
+      end
+    end
+    @target_line_temp = target_line_first.to_s + "-" + (target_line_first + tar_check_line).to_s + ";"
+  end
+
+  def set_compare_line(line)
+    # Take the line No. in compare file which is checked
+    compare_line_right = line.rindex(" path=\"" + @compare_path) - 2
+    compare_line_first_temp = line[12..compare_line_right]
+    compare_line_first = compare_line_first_temp.to_i
+
+    # マークラインに空行とコメント行を追加
+    com_check_line = @check_line
+    com_file = File.open(@compare_path, 'r:utf-8' )
+    k = 0
+    flag = false
+    com_file.each_with_index do |l, j|
+      k += 1
+      if j == compare_line_first
+        flag = true
+      end
+      if k == com_check_line
+        break
+      end
+      if flag
+        if l.strip.chars[0].nil? || l.strip.chars[0] == "#"
+          com_check_line += 1
+        end
+      end
+    end
+    @compare_line_temp = compare_line_first.to_s + "-" + (compare_line_first + com_check_line).to_s + ";"
   end
 
   # チェック類似度の表示
